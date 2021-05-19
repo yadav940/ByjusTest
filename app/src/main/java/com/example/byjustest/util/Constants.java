81beb8e4e9dbd1a6ccba3a4231906d0534c2f206
vinod yadav <vinod@modistabox.com>
2021-05-19 19:46:18 +0530

    news  details

--- app/src/main/java/com/example/byjustest/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.byjustest.util;
-
-public class Constants {
-    public static final String BASE_URL = "https://newsapi.org";
-    public static final String API_KEY = "e278e8b7d8404132a36fe1f1a2fde799";
-    public static final String COUNTRY_CODE = "in";
-    public static final String STATUS_FAILED = "FAILED";
-    public static final String STATUS_OK = "ok";
-}
